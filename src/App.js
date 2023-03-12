c88c5b71dcc902f7a22c444c3c34cc952f28c4be
julia <julichis@mail.ru>
2023-03-12 19:15:06 +0400

    created grid structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="results-component">
+      <div className="results-component__info"></div>
+      <div className="results-component__summary"></div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
