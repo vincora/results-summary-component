e8f50a1b19263d1326f9340534fa0e9cd381c5a5
julia <julichis@mail.ru>
2023-03-12 20:31:55 +0400

    grid left part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
      <div className="results-component">
-      <div className="results-component__info"></div>
-      <div className="results-component__summary"></div>
+      <div className="result">
+        <div className="result__title">Your Result</div>
+        <div className="result__score">
+          <div className="result__your-score">76</div>
+          <div className="result__total">of 100</div>
+        </div>
+        <div className="result__value">Great</div>
+        <div className="result__info">You scored higher than 65% of the people who have taken these tests.</div>
+      </div>
+      <div className="summary">555</div>
      </div>
     </div>
   );
